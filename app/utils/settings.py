8b013b0851daa3c807aa030a8c7e58a9d5dde2ac
Jorge Moreno <sistemas@raisacv.local>
2023-07-18 12:18:12 -0600

    Agregado el archivo de settings para obtener las variables de entorno para la base de datos

---
+++ app/utils/settings.py
@@ -0,0 +1,21 @@
+"""
+Configuraciones para obtener las variables de entorno
+"""
+import os
+
+from dotenv import load_dotenv
+from pydantic import BaseSettings
+
+# Cargamos las variables de entorno
+load_dotenv()
+
+class Settings(BaseSettings):
+    """
+    Clase Settings extiende de BaseSettings para
+    forzar los tipos de datos, en este caso str
+    """
+    db_name: str = os.getenv('DB_NAME')
+    db_user: str = os.getenv('DB_USER')
+    db_pass: str = os.getenv('DB_PASS')
+    db_host: str = os.getenv('DB_HOST')
+    db_port: str = os.getenv('DB_PORT')
